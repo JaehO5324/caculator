b3b42669f9998217e12255e8550c1357e0576d73
JaehO5324 <64696680+JaehO5324@users.noreply.github.com>
2020-06-21 14:03:52 +0900

    Add files via upload
    계산기 완성

--- Caculator.java
+++ Caculator.java
@@ -17,8 +17,8 @@ import javax.swing.JTextField;
 
 public class Caculator extends JFrame {
 
-	String[] arrStr = { "1", "2", "3", "-", "4", "5", "6", "+", "7", "8", "9", "*", "0", "/", "caculate", "reset",
-			"del" };
+	String[] arrStr = { "1", "2", "3", "-", "4", "5", "6", "+", "7", "8", "9", "*", "0", "/", "caculate", "del",
+			".","reset" };
 
 	Container cp;
 
@@ -26,14 +26,15 @@ public class Caculator extends JFrame {
 	String q[] = new String[1000], n;
 	String[] p = new String[50];
 
-	int ct = 0;
+	int ct = 0, ntext = 1,ncal=1;
 	double rs, fs;
-
+	JTextArea tf = new JTextArea();
+	
 	public Caculator() {
 
 		String rts[] = new String[1000];
 		cp = getContentPane();
-		JTextArea tf = new JTextArea();
+		
 		JButton[] num = new JButton[17];
 		// JButton[] pl = new JButton[4];
 		JPanel panel = new JPanel();
@@ -41,10 +42,12 @@ public class Caculator extends JFrame {
 		tf.setPreferredSize(new Dimension(200, 200));
 		tf.setLineWrap(true);
 		tf.setWrapStyleWord(true);
-         String ss;
+		tf.setEditable(false);
+		String ss;
 		setTitle("caculator");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(350, 500);
+		setResizable(false);
 		for (int i = 0; i <= 16; i++) {
 			final int s = i + 1;
 			num[i] = new JButton("" + arrStr[i]);
@@ -57,152 +60,159 @@ public class Caculator extends JFrame {
 					// TODO Auto-generated method stub
 
 					if (e.getActionCommand().equals("-")) {
-						if (!isCalBtn) {
-							isCalBtn = true;
-							if (!tf.getText().equals("")) {
-								q[ct] = "-";
-
-								p[ct] = tf.getText();
-
-								ct++;
-
-								tf.setText(tf.getText() + e.getActionCommand());
-							}
-						}
+						cal("-" , e);
 
 						System.out.println(e.getActionCommand());
 					} else if (e.getActionCommand().equals("+")) {
-						if (!isCalBtn) {
-							isCalBtn = true;
-							if (!tf.getText().equals("")) {
-								q[ct] = "+";
-
-								p[ct] = tf.getText();
-
-								ct++;
-								tf.setText(tf.getText() + e.getActionCommand());
-							}
-						}
+						cal("+" , e);
 
 					} else if (e.getActionCommand().equals("*")) {
-						if (!isCalBtn) {
-							isCalBtn = true;
-							if (!tf.getText().equals("")) {
-								q[ct] = "*";
-
-								p[ct] = tf.getText();
-
-								ct++;
-								tf.setText(tf.getText() + e.getActionCommand());
-							}
-						}
+						cal("*" , e);
 
 					} else if (e.getActionCommand().equals("/")) {
+						cal("/" , e);
+
+					}
+					else if(e.getActionCommand().equals("."))
+					{
 						if (!isCalBtn) {
 							isCalBtn = true;
 							if (!tf.getText().equals("")) {
-								q[ct] = "/";
-
-								p[ct] = tf.getText();
 								tf.setText(tf.getText() + e.getActionCommand());
-								ct++;
-
 							}
+							ntext = 1;
+                             ncal=1;
 						}
-
-					} else if (e.getActionCommand().equals("caculate")) {
-
+					}
+					
+					else if (e.getActionCommand().equals("caculate")) {
+						if(ncal==0)
+						{
+                         ncal=1;
 						String get[] = tf.getText().toString().split("(?<=[*/+-])|(?=[*/+-])");
-						
+
 						ArrayList<String> arrayList = new ArrayList<>();
+
 						for (int j = 0; j < get.length; j++) {
 							arrayList.add(get[j]);
-							System.out.print(arrayList.get(j)+"\n");
+							System.out.print(arrayList.get(j) + "\n");
 						}
-						
+
 						for (int j = 0; j < arrayList.size(); j++) {
-							if(arrayList.get(j).equals("*")){
-								
-								int result = Integer.parseInt(arrayList.get(j-1)) * Integer.parseInt(arrayList.get(j+1));
-								arrayList.set(j-1, result+"");
+							if (arrayList.get(j).equals("*")) {
+
+								double result = Double.parseDouble(arrayList.get(j - 1))
+										* Double.parseDouble(arrayList.get(j + 1));
+								arrayList.set(j - 1, result + "");
 								arrayList.remove(j);
 								arrayList.remove(j);
 								for (int k = 0; k < arrayList.size(); k++) {
-									System.out.print(arrayList.get(k)+"\n");
+									System.out.print(arrayList.get(k) + "\n");
 								}
-							}
-							else if(arrayList.get(j).equals("/")){
-								
-								int result = Integer.parseInt(arrayList.get(j-1)) /Integer.parseInt(arrayList.get(j+1));
-								arrayList.set(j-1, result+"");
+							} else if (arrayList.get(j).equals("/")) {
+
+								double result = Double.parseDouble(arrayList.get(j - 1))
+										/ Double.parseDouble(arrayList.get(j + 1));
+								arrayList.set(j - 1, result + "");
 								arrayList.remove(j);
 								arrayList.remove(j);
 								for (int k = 0; k < arrayList.size(); k++) {
-									System.out.print(arrayList.get(k)+"\n");
+									System.out.print(arrayList.get(k) + "\n");
 								}
 							}
 						}
 						for (int j = 0; j < arrayList.size(); j++) {
-							if(arrayList.get(j).equals("+")){
-								
-								int result = Integer.parseInt(arrayList.get(j-1)) + Integer.parseInt(arrayList.get(j+1));
-								arrayList.set(j-1, result+"");
+							if (arrayList.get(j).equals("+")) {
+
+								double result = Double.parseDouble(arrayList.get(j - 1))
+										+ Double.parseDouble(arrayList.get(j + 1));
+								arrayList.set(j - 1, result + "");
 								arrayList.remove(j);
 								arrayList.remove(j);
 								for (int k = 0; k < arrayList.size(); k++) {
-									System.out.print(arrayList.get(k)+"\n");
+									System.out.print(arrayList.get(k) + "\n");
 								}
-							}
-							else if(arrayList.get(j).equals("-")){
-								
-								int result = Integer.parseInt(arrayList.get(j-1)) - Integer.parseInt(arrayList.get(j+1));
-								arrayList.set(j-1, result+"");
+							} else if (arrayList.get(j).equals("-")) {
+
+								double result = Double.parseDouble(arrayList.get(j - 1))
+										- Double.parseDouble(arrayList.get(j + 1));
+								arrayList.set(j - 1, result + "");
 								arrayList.remove(j);
 								arrayList.remove(j);
-								int y=0;
+								int y = 0;
 								for (int k = 0; k < arrayList.size(); k++) {
-									System.out.print(arrayList.get(k)+"\n");
+									System.out.print(arrayList.get(k) + "\n");
 									y++;
 								}
 							}
-						
-						
+
+						}
+
+						tf.setText(arrayList.get(0) + "");
+						ntext = 0;
+
 					}
-					
-					tf.setText(arrayList.get(0)+"");
 					}
 
 					else if (e.getActionCommand().equals("reset")) {
 						tf.setText("");
+						ntext = 1;
+						ncal=1;
 					} else if (e.getActionCommand().equals("del")) {
-					    String es;
-						es=tf.getText().substring(tf.getText().toString().length()-1,tf.getText().toString().length());
-						tf.setText(tf.getText().substring(0,tf.getText().toString().length()-1));
-						
-				if(es.equals("+")||es.equals("-")||es.equals("*")||es.equals("/")){
-					ct--;
-					}
+						String es;
+						es = tf.getText().substring(tf.getText().toString().length() - 1,
+								tf.getText().toString().length());
+						tf.setText(tf.getText().substring(0, tf.getText().toString().length() - 1));
+
+						if (es.equals("+") || es.equals("-") || es.equals("*") || es.equals("/")) {
+							ct--;
+						}
 
 					} else {
 						isCalBtn = false;
+						ncal=0;
+						if (ntext == 1)
+							tf.setText(tf.getText() + e.getActionCommand());
 
-						tf.setText(tf.getText() + e.getActionCommand());
+						else if (ntext == 0) {
+							tf.setText("");
+							tf.setText(tf.getText() + e.getActionCommand());
+							ntext = 1;
+						}
 
 					}
 
-					
 				}
 			});
 
-
 		}
 
+		{
+
+		}
 		cp.setLayout(new BorderLayout());
 		panel.setLayout(new GridLayout(5, 3));
 		cp.add(panel, "Center");
 
 		cp.add(tf, "North");
 
+	}
+	
+	public void cal(String get , ActionEvent e){
+		if (!isCalBtn) {
+			isCalBtn = true;
+			if (!tf.getText().equals("")) {
+				q[ct] = get;
+
+				p[ct] = tf.getText();
+
+				ct++;
+
+				tf.setText(tf.getText() + e.getActionCommand());
+			}
+			ntext = 1;
+			ncal=1;
+		}
 	}
 
 	public static void main(String[] args) {
